7abde7046f834b9db825635178035c1d4170a650
Sander Verweij <sverweij@users.noreply.github.com>
2022-07-23 12:27:10 +0200

    feature: makes title width depend on available space (#3)

--- src/terse-advisory-log.js
+++ src/terse-advisory-log.js
@@ -1,9 +1,19 @@
 import { createHash } from "node:crypto";
 
+/**
+ *
+ * @param {import("../types/compact-yarn-audit").ITerseEntry} pObject
+ * @returns {string}
+ */
 function hash(pObject) {
   return createHash("md5").update(JSON.stringify(pObject)).digest("base64");
 }
 
+/**
+ *
+ * @param {any} pLogEntry entry as emitted by yarn audit
+ * @returns {import("../types/compact-yarn-audit").ITerseEntry}
+ */
 function extractUsefulAttributes(pLogEntry) {
   const lFixable = pLogEntry.data.advisory.patched_versions !== "<0.0.0";
   const lVia = pLogEntry.data.resolution.path.split(">").shift();
@@ -19,6 +29,11 @@ function extractUsefulAttributes(pLogEntry) {
     via: lVia === pLogEntry.data.advisory.module_name ? "." : lVia,
   };
 }
+/**
+ *
+ * @param {import("../types/compact-yarn-audit").SeverityType} pSeverity
+ * @returns {number}
+ */
 function severity2Order(pSeverity) {
   const lSeverity2Order = {
     critical: 1,
@@ -30,10 +45,21 @@ function severity2Order(pSeverity) {
   return lSeverity2Order[pSeverity] || -1;
 }
 
+/**
+ *
+ * @param {import("../types/compact-yarn-audit").ITerseEntry} pEntry
+ * @returns {string}
+ */
 function getKey(pEntry) {
   return `${severity2Order(pEntry.severity)}|${pEntry.module_name}`;
 }
 
+/**
+ *
+ * @param {import("../types/compact-yarn-audit").ITerseEntry} pEntryLeft
+ * @param {import("../types/compact-yarn-audit").ITerseEntry} pEntryRight
+ * @returns {number}
+ */
 function orderEntry(pEntryLeft, pEntryRight) {
   return getKey(pEntryLeft) > getKey(pEntryRight) ? 1 : -1;
 }
@@ -49,13 +75,17 @@ export class TerseAdvisoryLog {
     if (pEntry.type === "auditAdvisory") {
       const lUsefulAttributes = extractUsefulAttributes(pEntry);
 
-      // Some auditlogs are several gigabytes long. Given that there'll
+      // Some audit logs are several gigabytes long. Given that there'll
       // be quite some duplicates, the overhead of the hash will be negligable
       // compared to the amount of memory that'd normally be needed
       this.log.set(hash(lUsefulAttributes), lUsefulAttributes);
     }
   }
 
+  /**
+   *
+   * @returns {import("../types/compact-yarn-audit").ITerseEntry[]}
+   */
   get() {
     return [...this.log.values()].sort(orderEntry);
   }
