83a24aa409cf5b456c96647f6b00a541ad931c61
sverweij <sverweij@users.noreply.github.com>
2024-04-13 13:41:43 +0200

    chore: tabs < spaces

--- dist/index.js
+++ dist/index.js
@@ -4,15 +4,15 @@ import { TerseAdvisoryLog } from "./terse-advisory-log.js";
 import format from "./format.js";
 const lAdvisoryLog = new TerseAdvisoryLog();
 process.stdin
-    .pipe(ndjson.parse())
-    .on("data", (pLogEntry) => {
-    lAdvisoryLog.add(pLogEntry);
-})
-    .on("error", (pError) => {
-    console.error(pError);
-    process.exitCode = 1;
-    process.exit();
-})
-    .on("end", () => {
-    console.log(format(lAdvisoryLog.get()));
-});
+	.pipe(ndjson.parse())
+	.on("data", (pLogEntry) => {
+		lAdvisoryLog.add(pLogEntry);
+	})
+	.on("error", (pError) => {
+		console.error(pError);
+		process.exitCode = 1;
+		process.exit();
+	})
+	.on("end", () => {
+		console.log(format(lAdvisoryLog.get()));
+	});
--- dist/terse-advisory-log.js
+++ dist/terse-advisory-log.js
@@ -1,47 +1,47 @@
 function extractUsefulAttributes(pLogEntry) {
-    const lFixable = pLogEntry.data.advisory.patched_versions !== "<0.0.0";
-    const lVia = pLogEntry.data.resolution.path.split(">").shift() ?? "?";
-    return {
-        severity: pLogEntry.data.advisory.severity,
-        title: pLogEntry.data.advisory.title,
-        fixable: lFixable,
-        fixString: lFixable
-            ? `"${pLogEntry.data.advisory.module_name}": "${pLogEntry.data.advisory.patched_versions}"`
-            : "no fix available",
-        module_name: pLogEntry.data.advisory.module_name,
-        via: lVia === pLogEntry.data.advisory.module_name ? "." : lVia,
-    };
+	const lFixable = pLogEntry.data.advisory.patched_versions !== "<0.0.0";
+	const lVia = pLogEntry.data.resolution.path.split(">").shift() ?? "?";
+	return {
+		severity: pLogEntry.data.advisory.severity,
+		title: pLogEntry.data.advisory.title,
+		fixable: lFixable,
+		fixString: lFixable
+			? `"${pLogEntry.data.advisory.module_name}": "${pLogEntry.data.advisory.patched_versions}"`
+			: "no fix available",
+		module_name: pLogEntry.data.advisory.module_name,
+		via: lVia === pLogEntry.data.advisory.module_name ? "." : lVia,
+	};
 }
 function severity2Order(pSeverity) {
-    const lSeverity2Order = {
-        critical: 1,
-        high: 2,
-        moderate: 3,
-        low: 4,
-        info: 5,
-    };
-    return lSeverity2Order[pSeverity] || -1;
+	const lSeverity2Order = {
+		critical: 1,
+		high: 2,
+		moderate: 3,
+		low: 4,
+		info: 5,
+	};
+	return lSeverity2Order[pSeverity] || -1;
 }
 function getKey(pEntry) {
-    return `${severity2Order(pEntry.severity)}|${pEntry.module_name}`;
+	return `${severity2Order(pEntry.severity)}|${pEntry.module_name}`;
 }
 function orderEntry(pEntryLeft, pEntryRight) {
-    return getKey(pEntryLeft) > getKey(pEntryRight) ? 1 : -1;
+	return getKey(pEntryLeft) > getKey(pEntryRight) ? 1 : -1;
 }
 export class TerseAdvisoryLog {
-    log = new Set();
-    constructor() {
-        this.log = new Set();
-    }
-    add(pEntry) {
-        if (pEntry.type === "auditAdvisory") {
-            const lUsefulAttributes = extractUsefulAttributes(pEntry);
-            this.log.add(JSON.stringify(lUsefulAttributes));
-        }
-    }
-    get() {
-        return Array.from(this.log)
-            .map((pStringifiedEntry) => JSON.parse(pStringifiedEntry))
-            .sort(orderEntry);
-    }
+	log = new Set();
+	constructor() {
+		this.log = new Set();
+	}
+	add(pEntry) {
+		if (pEntry.type === "auditAdvisory") {
+			const lUsefulAttributes = extractUsefulAttributes(pEntry);
+			this.log.add(JSON.stringify(lUsefulAttributes));
+		}
+	}
+	get() {
+		return Array.from(this.log)
+			.map((pStringifiedEntry) => JSON.parse(pStringifiedEntry))
+			.sort(orderEntry);
+	}
 }
